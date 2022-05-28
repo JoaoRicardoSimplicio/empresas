d2785b44b13b71ea10353e4a9f6be1c46018bb41
João Ricardo Simplício Soares <joaoricardosimplicio16@gmail.com>
2022-05-28 11:13:53 -0300

    Cria model Empresa

--- empresas/models.py
+++ empresas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Empresa(models.Model):
+
+    comprador = models.CharField(max_length=110)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    quantidade = models.IntegerField()
+    endereco = models.TextField()
+
+    def __str__(self):
+        return self.comprador
